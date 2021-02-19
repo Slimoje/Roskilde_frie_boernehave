e7c7c3da619f65527897cc385ab5091ff80520fd
Slimoje <mart187c@stud.kea.dk>
2021-02-19 13:48:22 +0100

    test x

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,17 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
     //Fungerer det nu?
+
+
+
+    }
+    public static void createDays(Scanner scan){
 
 
 
--- src/com/company/WorkHours.java
+++ src/com/company/WorkHours.java
@@ -16,4 +16,5 @@ public class WorkHours {
     public ArrayList<Day> getDays() {
         return days;
     }
+    //lalala
 }
