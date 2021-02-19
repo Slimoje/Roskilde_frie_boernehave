3e6a26ae8704ab189da895219c9fb35112e208a1
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2021-02-19 14:10:18 +0100

    added weeknumber

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) {
-    //Fungerer det nu?
+
 
 
 
--- src/com/company/WorkHours.java
+++ src/com/company/WorkHours.java
@@ -3,17 +3,35 @@ package com.company;
 import java.util.ArrayList;
 
 public class WorkHours {
+    int weekNumber;
     ArrayList<Day> days;
 
-    public WorkHours(ArrayList<Day> list) {
-        days = list;
+    public WorkHours(int weekNumber, ArrayList<Day> days) {
+        this.weekNumber = weekNumber;
+        this.days = days;
+    }
+
+    public int getWeekNumber() {
+        return weekNumber;
     }
 
     public ArrayList<Day> getDays() {
         return days;
     }
 
+    public void setWeekNumber(int weekNumber) {
+        this.weekNumber = weekNumber;
+    }
+
     public void setDays(ArrayList<Day> days) {
         this.days = days;
+    }
+
+    @Override
+    public String toString() {
+        return "WorkHours{" +
+                "weekNumber=" + weekNumber +
+                ", days=" + days +
+                '}';
     }
 }
